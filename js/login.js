a827ca32167817a067ec5990ec681fd8b7d741ef
Maria Eduarda <mfs6401131@gmail.com>
2023-01-04 16:27:33 -0300

    inserindo o visualizar senha

--- js/login.js
+++ js/login.js
@@ -1,5 +1,20 @@
 'use strict'
 
+//Ocultar Senha
+const inputPassword = document.querySelector('#password', '#password2') 
+const input2 = document.querySelector('#password2')
+
+function showHide () {
+    if (inputPassword.type === 'password' && input2.type === 'password'){
+        inputPassword.setAttribute('type', 'text')
+        input2.setAttribute('type', 'text')
+    } else{
+        inputPassword.setAttribute('type', 'password')
+        input2.setAttribute('type', 'password')
+    }
+}
+
+//Animação -> mover container 
 const loginContainer = document.getElementById('login-container')
 
 const move = () => loginContainer.classList.toggle('move') //se tiver a class move o toogle remove, se não ele adiciona
